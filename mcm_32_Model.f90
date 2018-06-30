590474376b60d70e4d8f690b170cf5e1d86fcf88
Willi Schimmel <schimmel@tropos.de>
2018-06-30 15:01:08 +0200

    first commit
      new MCM 3.2 gas phase chemistry version generated by KPP

---
+++ mcm_32_Model.f90
@@ -0,0 +1,20 @@
+MODULE mcm_32_Model
+
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!  Completely defines the model mcm_32
+!    by using all the associated modules
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  USE mcm_32_Precision
+  USE mcm_32_Parameters
+  USE mcm_32_Global
+  USE mcm_32_Function
+  USE mcm_32_Integrator
+  USE mcm_32_Rates
+  USE mcm_32_Jacobian
+  USE mcm_32_LinearAlgebra
+  USE mcm_32_Monitor
+  USE mcm_32_Util
+
+END MODULE mcm_32_Model
+
